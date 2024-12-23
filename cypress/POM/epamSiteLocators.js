c246badbb1c05620925674e76ad20b1c736fcdf9
nenadInforma <nenad.damjanovic@informa.com>
2024-12-23 17:34:40 +0100

    locators reverted

--- cypress/POM/epamSiteLocators.js
+++ cypress/POM/epamSiteLocators.js
@@ -1,111 +1,111 @@
 
 class epamSite{
       
-        get cookyConsentButton() {
+        cookyConsentButton() {
           return cy.get('#onetrust-accept-btn-handler');
         }
 
-        get dropdownMenu() {
+        dropdownMenu() {
             return cy.get('.hamburger-menu__button');
         }
 
-        get aboutLink() {
+        aboutLink() {
             return cy.get('[href="/about"]');
         }
 
-        get downloadDocButton(){
+        downloadDocButton(){
             return cy.get('[href="https://www.epam.com/content/dam/epam/free_library/EPAM_Corporate_Overview_Q4_EOY.pdf"]');
         }
 
-        get docRouth() {
+        docRouth() {
             return cy.readFile('/Users/Nenad_Damjanovic/Desktop/SQE_TASK_1/cypress/downloads/EPAM_Corporate_Overview_Q4_EOY.pdf');
         }
 
-        get epamLogo() {
+        epamLogo() {
             return cy.get('[class="header__logo header__logo-light"]');
         }
 
-        get contacUsButton() {
+        contacUsButton() {
             return cy.get('[class="button__content button__content--desktop"]').contains('CONTACT US');
         }
 
-        get submitButton() {
+        submitButton() {
             return cy.get('[type="submit"]');
         }
 
-        get firstNameError() {
+        firstNameError() {
             return cy.get('#_content_epam_en_about_who-we-are_contact_jcr_content_content-container_section_section-par_form_constructor_user_first_name-error');
         }
 
-        get lastNameError() {
+        lastNameError() {
             return cy.get('#_content_epam_en_about_who-we-are_contact_jcr_content_content-container_section_section-par_form_constructor_user_last_name-error');
         }
 
-        get emailError() {
+        emailError() {
             return cy.get('#_content_epam_en_about_who-we-are_contact_jcr_content_content-container_section_section-par_form_constructor_user_email-error');
         }
 
-        get phoneError() {
+        phoneError() {
             return cy.get('#_content_epam_en_about_who-we-are_contact_jcr_content_content-container_section_section-par_form_constructor_user_phone-error');
         }
 
-        get searchIcon() {
+        searchIcon() {
             return cy.get('[class="search-icon dark-icon header-search__search-icon"]');
         }
 
-        get searchInputField() {
+        searchInputField() {
             return cy.get('#new_form_search');
         }
 
-        get findButton() {
+        findButton() {
             return cy.get('.bth-text-layer');
         }
 
-        get searchResultsCounter() {
+        searchResultsCounter() {
             return cy.get('.search-results__counter');
         }
 
-        get sectionTitleHeader() {
+        sectionTitleHeader() {
             return cy.get('.museo-sans-light');
         }
 
-        get locationTabs() {
+        locationTabs() {
             return cy.get('[role="tab"]');
         }
 
-        get locationCountryList() {
+        locationCountryList() {
             return cy.get('[class="locations-viewer-23__country-title list"]');
         }
 
-        get footerTitles() {
+        footerTitles() {
             return cy.get('[class="heading large-preheader"]');
         }
 
-        get leftSectionPolicieList() {
+        leftSectionPolicieList() {
             return cy.get('[class="ul policies-left"] li');
         }
 
-        get rightSectionPolicieList() {
+        rightSectionPolicieList() {
             return cy.get('[class="ul policies-right"] li');
         }
 
-        get languageButton() {
+        languageButton() {
             return cy.get('.location-selector__button-language');
         }
 
-        get languageUK() {
+        languageUK() {
             return  cy.get('[lang="uk"]');
         }
 
-        get themeSwitcher() {
+        themeSwitcher() {
             return  cy.get('.theme-switcher');
         }
 
-        get themeTitle() {
+        themeTitle() {
             return  cy.get('[class="theme-switcher-label light-mode-label-text body-text-small"]');
         }
 
-        get headerTitle() {
+        headerTitle() {
             return  cy.get('head title');
         }
 }
